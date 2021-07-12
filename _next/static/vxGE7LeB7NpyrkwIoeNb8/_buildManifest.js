29c1cc60a103acb8308701b8171752b119e54cc6
baealex <baealex@hey.com>
2021-07-12 14:38:29 +0000

    Deploying to gh-pages from  @ ea6bb6b558ba5d22a8fc9008272b1566739fa5e1 🚀

---
+++ _next/static/vxGE7LeB7NpyrkwIoeNb8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b24579dd4f0d47c4311b.js"],"/_error":["static/chunks/pages/_error-94ed2348718d59e1af9b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
